8759b1da72c3454490765c5ad2965a5da273e12d
Tomas Desktop <tomasngy@gmail.com>
2015-01-21 19:32:43 +0100

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Tomas Nagy on 21/01/2015.
+ */
+var livereload = require('gulp-livereload'),
+    gulp = require('gulp');
+
+gulp.task('livereloader', function() {
+    livereload.listen();
+    gulp.watch(['public/**/*.js', 'public/**/*.css', 'views/*.ejs']).on('change', livereload.changed);
+});
