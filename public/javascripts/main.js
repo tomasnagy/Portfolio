e2b7f8b87fdad70fee7005f43e7079a3c20951c9
Tomas Desktop <tomasngy@gmail.com>
2015-02-13 16:05:29 +0100

    changed data location

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,7 +5,7 @@
     var request;
 
     request = new XMLHttpRequest();
-    request.open('GET', window.location.host + '/data/data.json', true);
+    request.open('GET', window.location.host + 'javascripts/data/data.json', true);
     request.addEventListener('load', function () {
         if (request.status >= 200 && request.status <= 400) {
             var data = JSON.parse(request.responseText);
